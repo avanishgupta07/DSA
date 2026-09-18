d71c3d165745ec91c0c8e2273f456cd7f5c59820
Avanish Kumar Gupta <avanishguptaaza@gmail.com>
2026-09-18 18:59:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (11.49%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int start = 0;
+        int mid = 0;
+        int right = nums.size() - 1;
+        while (mid <= right) {
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[start]);
+                start++;
+                mid++;
+            }
+            else if (nums[mid] == 2) {
+                swap(nums[mid], nums[right]);
+                right--;
+            }   else {
+                mid++;
+            }
+        }
+    }
+};
